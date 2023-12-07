d40a07efc7aaa5302f6432d71613bf615e413bc7
truong.nguyen3 <truong.nguyen3@niteco.se>
2023-12-07 10:56:39 +0700

    init

--- src/navigators/Application.tsx
+++ src/navigators/Application.tsx
@@ -1,7 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { Example, Startup } from '@/screens';
+import { Example, Startup, BgActions } from '@/screens';
 import { useTheme } from '@/theme';
 
 import type { ApplicationStackParamList } from '@/types/navigation';
@@ -14,6 +14,7 @@ function ApplicationNavigator() {
 	return (
 		<NavigationContainer theme={navigationTheme}>
 			<Stack.Navigator key={variant} screenOptions={{ headerShown: false }}>
+				<Stack.Screen name="BgActions" component={BgActions} />
 				<Stack.Screen name="Startup" component={Startup} />
 				<Stack.Screen name="Example" component={Example} />
 			</Stack.Navigator>
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -3,6 +3,7 @@ import type { StackScreenProps } from '@react-navigation/stack';
 export type ApplicationStackParamList = {
 	Startup: undefined;
 	Example: undefined;
+	BgActions: undefined;
 };
 
 export type ApplicationScreenProps =
